a0e30c511b552e63c21d418a269f37619ba3d92e
bartek <taras_basket@wp.pl>
2024-01-23 18:28:32 +0100

    creating SLactions, setting reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++
@@ -1,8 +0,0 @@
-import { createReducer } from '@ngrx/store';
-import { Ingredient } from '../shared/ingredient.model';
-
-const initialState = {
-  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
-};
-
-export const shoppingListReducer = createReducer(initialState);
---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer } from '@ngrx/store';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
+
+const initialState = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+  editIndex: -1,
+};
+
+export const shoppingListReducer = createReducer(
+  initialState,
+  on(ShoppingListActions.addIngredient, (state, action) => ({
+    ...state,
+    ingredients: state.ingredients.concat(action.ingredient),
+  }))
+);
