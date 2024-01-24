992bcb6a46d85a967503e2965194a8743859e948
bartek <taras_basket@wp.pl>
2024-01-24 18:15:48 +0100

    addIngredients added to slActions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,3 +5,8 @@ export const addIngredient = createAction(
   '[Shopping List] Add Ingredient',
   props<{ ingredient: Ingredient }>()
 );
+
+export const addIngredients = createAction(
+  '[Shopping List] Add Ingredients',
+  props<{ ingredients: Ingredient[] }>()
+);
