f365afad93694c5328b2717acb135eac62027974
bartek <taras_basket@wp.pl>
2024-01-25 18:29:16 +0100

    adding new actions to sl; startEdit and stopEdit

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -21,3 +21,12 @@ export const updateIngredient = createAction(
 export const deleteIngredient = createAction(
   '[Shopping List] Delete Ingredient'
 );
+
+export const startEdit = createAction(
+  '[Shopping List] Start Edit',
+  props<{
+    index: number;
+  }>()
+);
+
+export const stopEdit = createAction('[Shopping List] Stop Edit');
