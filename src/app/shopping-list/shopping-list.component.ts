e4b515134ab2f7a584e1e22296de47993854721e
bartek <taras_basket@wp.pl>
2024-01-25 18:34:17 +0100

    chaning metodology of onEditItem method in slComponent

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,7 @@ import { ShoppingListService } from './shopping-list.service';
 import { LoggingService } from '../logging.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -28,7 +29,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(ShoppingListActions.startEdit({ index }));
   }
 
   ngOnDestroy() {
