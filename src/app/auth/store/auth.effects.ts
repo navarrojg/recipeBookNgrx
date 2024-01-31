e5c0fe9b5bf66507683e77341b70af6ce7dad061
bartek <taras_basket@wp.pl>
2024-01-31 20:28:23 +0100

    creating interface AuthResponseData

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -9,3 +9,13 @@ import { environment } from '../../../environments/environment';
 import * as AuthActions from './auth.actions';
 import { User } from '../user.model';
 import { AuthService } from '../auth.service';
+
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+  }
