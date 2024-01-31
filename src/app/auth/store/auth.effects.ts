950cb083f2252e985b049eb63dc7df58c99c45e8
bartek <taras_basket@wp.pl>
2024-01-31 20:27:54 +0100

    adding all necessary imports to authEffects

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { switchMap, catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+import * as AuthActions from './auth.actions';
+import { User } from '../user.model';
+import { AuthService } from '../auth.service';
