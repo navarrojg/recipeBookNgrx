a55044273a25579147ff7a397af9d248d332c42f
bartek <taras_basket@wp.pl>
2024-02-05 20:04:57 +0100

    cleaning authService, updating setLogoutTimer method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { catchError, tap } from 'rxjs/operators';
-import { throwError, BehaviorSubject } from 'rxjs';
-import { environment } from '../../environments/environment';
-
-import { User } from './user.model';
 import { Store } from '@ngrx/store';
 
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
 
-
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
   constructor(private store: Store<fromApp.AppState>) {}
 
-  setLogoutTimer(expirationDuration: number) {}
+  setLogoutTimer(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.store.dispatch(AuthActions.logout());
+    }, expirationDuration);
+  }
 
   clearLogoutTimer() {
     if (this.tokenExpirationTimer) {
