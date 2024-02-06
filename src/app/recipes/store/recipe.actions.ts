3213779021111f5543f7c06678097d4e176e15e9
bartek <taras_basket@wp.pl>
2024-02-06 22:28:13 +0100

    created action setRecipes in recipeActions

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -11,3 +11,10 @@ export const addRecipe = createAction(
 export const fetchRecipes = createAction('[Recipe] Fetch Recipes');
 
 export const storeRecipes = createAction('[Recipe] Store Recipes');
+
+export const setRecipes = createAction(
+  '[Recipe] Set Recipes',
+  props<{
+    recipes: Recipe[];
+  }>()
+);
