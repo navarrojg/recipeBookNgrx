400f68333c1af3917070545f72cc8fb051ab173c
bartek <taras_basket@wp.pl>
2024-02-06 22:29:38 +0100

    adding setRecipes action to recipeReducer

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,6 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { Recipe } from '../recipe.model';
-// import * as RecipesActions from '../store/recipe.actions';
+import * as RecipesActions from '../store/recipe.actions';
 
 export interface State {
   recipes: Recipe[];
@@ -10,7 +10,13 @@ const initialState: State = {
   recipes: [],
 };
 
-const _recipeReducer = createReducer(initialState);
+const _recipeReducer = createReducer(
+  initialState,
+  on(RecipesActions.setRecipes, (state, action) => ({
+    ...state,
+    recipes: [...action.recipes],
+  }))
+);
 
 export function recipeReducer(state: State, action: Action) {
   return _recipeReducer(state, action);
