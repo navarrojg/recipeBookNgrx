8fa5206e23be4b94de75cf7eb6343588cde22340
bartek <taras_basket@wp.pl>
2024-02-07 22:26:07 +0100

    updating recipesResolver logic

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,27 +2,37 @@ import { Injectable } from '@angular/core';
 import {
   Resolve,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot
+  RouterStateSnapshot,
 } from '@angular/router';
-
+import { take, map, switchMap } from 'rxjs/operators';
+import { Actions, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { Recipe } from './recipe.model';
-import { DataStorageService } from '../shared/data-storage.service';
-import { RecipeService } from './recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: 'root' })
-export class RecipesResolverService implements Resolve<Recipe[]> {
+export class RecipesResolverService implements Resolve<{ recipes: Recipe[] }> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipesService: RecipeService
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipesService.getRecipes();
-
-    if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-    } else {
-      return recipes;
-    }
+    return this.store.select('recipes').pipe(
+      take(1),
+      map((recipesState) => {
+        return recipesState.recipes;
+      }),
+      switchMap((recipes) => {
+        if (recipes.length === 0) {
+          this.store.dispatch(RecipesActions.fetchRecipes());
+          return this.actions$.pipe(ofType(RecipesActions.setRecipes), take(1));
+        } else {
+          return of({ recipes });
+        }
+      })
+    );
   }
 }
