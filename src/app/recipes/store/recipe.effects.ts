7f38362e36c647b294aaae622399985c53ec4fd3
bartek <taras_basket@wp.pl>
2024-02-07 20:40:57 +0100

    preparing recipeEffects file

---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { HttpClient } from '@angular/common/http';
+import { switchMap, map, withLatestFrom } from 'rxjs/operators';
+
+import * as RecipesActions from './recipe.actions';
+import { Recipe } from '../recipe.model';
+import * as fromApp from '../../store/app.reducer';
+
+@Injectable()
+export class RecipeEffects {
+
+
+    
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private store: Store<fromApp.AppState>
+  ) {}
+}
