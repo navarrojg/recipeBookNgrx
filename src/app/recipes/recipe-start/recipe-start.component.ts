b4f3f71751ab3e2b4c047803a288e0a777e5646d
bartek <taras_basket@wp.pl>
2024-02-21 20:09:30 +0100

    updating ui in recipeStartComponent, styling img in recipeDetailComponent

--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-start',
   templateUrl: './recipe-start.component.html',
-  styleUrls: ['./recipe-start.component.css']
+  styleUrls: ['./recipe-start.component.css'],
 })
 export class RecipeStartComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[];
+  subscription: Subscription;
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map((recipesState) => recipesState.recipes))
+      .subscribe((recipes: Recipe[]) => {
+        this.recipes = recipes;
+      });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
